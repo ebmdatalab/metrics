a2ba772b912191cfa46e8dc53a8f292d7c306533
George Hickman <george@ghickman.co.uk>
2023-11-02 10:09:44 +0000

    Move GitHub subcommand down into the github module
    We're about to add more commands so we don't want to have them all in
    the same cli.py.  This moves us to having a cli.py for each sub command,
    which is then registered in the top level cli.py.

--- metrics/cli.py
+++ metrics/cli.py
@@ -1,6 +1,6 @@
 import click
 
-from .github import commands as github_commands
+from .github.cli import github
 from .logs import setup_logging
 
 
@@ -15,32 +15,4 @@ def cli(ctx, debug):
     ctx.obj["DEBUG"] = debug
 
 
-@cli.group()
-@click.option("--token", required=True, envvar="GITHUB_TOKEN")
-@click.pass_context
-def github(ctx, token):
-    ctx.ensure_object(dict)
-
-    ctx.obj["TOKEN"] = token
-
-
-@github.command()
-@click.argument("org")
-@click.argument("date", type=click.DateTime())
-@click.option("--days-threshold", type=int)
-@click.pass_context
-def pr_queue(ctx, org, date, days_threshold):
-    date = date.date()
-
-    github_commands.pr_queue(org, date, days_threshold)
-
-
-@github.command()
-@click.argument("org")
-@click.argument("date", type=click.DateTime())
-@click.option("--days", default=7, type=int)
-@click.pass_context
-def pr_throughput(ctx, org, date, days):
-    date = date.date()
-
-    github_commands.pr_throughput(org, date, days)
+cli.add_command(github)
