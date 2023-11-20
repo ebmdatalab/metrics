654cebe82cea9ea490463d57085f62e93b3619f1
George Hickman <george@ghickman.co.uk>
2023-11-20 14:51:58 +0000

    Add engine test fixture
    We want to test some of the code against the database it's executing
    against.  This fixture runs before (and then after) the full test suite,
    creating a test database and dropping it when we're done.
    
    In the case of the test db still existing it removes it so we're always
    starting fresh.

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.engine import make_url
+from sqlalchemy_utils import create_database, database_exists, drop_database
+
+from metrics.timescaledb.writer import TIMESCALEDB_URL
+
+
+@pytest.fixture(scope="session", autouse=True)
+def engine():
+    # build a sqlalchemy.URL from the TIMESCALEDB_URL env var but prepend test_
+    # to the database name
+    url = make_url(TIMESCALEDB_URL)
+    url = url.set(database=f"test_{url.database}")
+
+    # drop the database if it already exists so we start with a clean slate.
+    if database_exists(url):  # pragma: no cover
+        drop_database(url)
+
+    create_database(url)
+
+    yield create_engine(url)
+
+    # drop the database on test suite exit
+    drop_database(url)
