81b93c66165b86ff35d79aedff0edf0ab79d9946
Lucy Bridges <lucy.bridges@thedatalab.org>
2024-01-12 16:27:29 +0000

    Create separate query module for graphql queries

---
+++ metrics/github/query.py
@@ -0,0 +1,18 @@
+def query_repos(client):
+    query = """
+    query repos($cursor: String, $org: String!) {
+      organization(login: $org) {
+        repositories(first: 100, after: $cursor) {
+          nodes {
+            name
+            archivedAt
+          }
+          pageInfo {
+              endCursor
+              hasNextPage
+          }
+        }
+      }
+    }
+    """
+    return client.get_query(query, path=["organization", "repositories"])
