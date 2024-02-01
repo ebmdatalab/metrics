33fa764b89108a31b7e2331fd08de17a8bb66c33
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2024-02-01 16:07:25 +0000

    Move all PR metrics logic into the prs module
    This doesn't achieve a lot, but it's the prelude to a refactoring that
    will involve inlining all the existing functions in prs, so it's easier
    to do that with all the code in one place.

--- metrics/tasks/prs.py
+++ metrics/tasks/prs.py
@@ -5,7 +5,7 @@ import structlog
 
 from metrics import timescaledb
 from metrics.github.client import GitHubClient
-from metrics.github.prs import fetch_prs, old_prs, pr_throughput
+from metrics.github.prs import get_metrics
 
 
 log = structlog.get_logger()
@@ -13,21 +13,17 @@ log = structlog.get_logger()
 
 def main():
     ebmdatalab_token = os.environ["GITHUB_EBMDATALAB_TOKEN"]
-    os_core_token = os.environ["GITHUB_OS_CORE_TOKEN"]
+    log.info("Working with org: ebmdatalab")
+    ebmdatalab_metrics = get_metrics(GitHubClient(ebmdatalab_token), "ebmdatalab")
 
-    ebmdatalab_metrics = get_metrics("ebmdatalab", ebmdatalab_token)
-    os_core_metrics = get_metrics("opensafely-core", os_core_token)
+    os_core_token = os.environ["GITHUB_OS_CORE_TOKEN"]
+    log.info("Working with org: opensafely-core")
+    os_core_metrics = get_metrics(GitHubClient(os_core_token), "opensafely-core")
+
     metrics = ebmdatalab_metrics + os_core_metrics
 
     timescaledb.reset_table(timescaledb.GitHubPullRequests)
     timescaledb.write(timescaledb.GitHubPullRequests, metrics)
-
-
-def get_metrics(org, ebmdatalab_token):
-    client = GitHubClient(ebmdatalab_token)
-    log.info(f"Working with org: {org}")
-    prs = fetch_prs(client, org)
-    return old_prs(prs, days_threshold=7) + pr_throughput(prs)
 
 
 if __name__ == "__main__":
