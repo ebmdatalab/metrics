4fd27c7994699890cfe63bc28dbdf723cea9cc41
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2024-02-03 11:51:07 +0000

    Calculate PR metrics daily rather than weekly
    This makes the logic for calculating metrics much simpler.
    
    Two consequences:
    * The chart definitions need to change to only display weekly values and
      correctly aggregate over those weekly windows.
    * This is very inefficient because of the cartesian product of PRs and
      days, but I've got an optimization coming for that.

--- metrics/tools/dates.py
+++ metrics/tools/dates.py
@@ -8,24 +8,10 @@ def date_from_iso(value):
     return datetime.fromisoformat(value).date()
 
 
-def iter_days(start, end, step=timedelta(days=1)):
+def iter_days(start, end):
     """
-    Days from start to end, inclusive. Single day steps by default.
+    Days from start to end, inclusive.
     """
     while start <= end:
         yield start
-        start += step
-
-
-def next_weekday(date_, weekday):
-    """
-    Get the date of the next {Mon,Tue,Wed,Thu,Fri,Sat,Sun}day on or following the given date.
-
-    The weekday is an integer, Monday being 0.
-    """
-    output = date_
-
-    while output.weekday() != weekday:
-        output += timedelta(days=1)
-
-    return output
+        start += timedelta(days=1)
--- tests/metrics/github/test_prs.py
+++ tests/metrics/github/test_prs.py
@@ -2,36 +2,35 @@ from datetime import date, timedelta
 
 import pytest
 
-from metrics.github.prs import calculate_counts, is_old, was_merged_in_week_ending
+from metrics.github.prs import calculate_counts, is_old, was_merged_on
 from metrics.github.query import FrozenDict
 
 
-TODAY = date(year=2023, month=12, day=1)  # a Friday
-LAST_MONDAY = date(year=2023, month=11, day=27)  # a Monday
-RECENTLY = date(year=2023, month=11, day=24)  # just a few days before the Monday
+TODAY = date(year=2023, month=6, day=10)
+YESTERDAY = date(year=2023, month=6, day=9)
+TOMORROW = date(year=2023, month=6, day=11)
+TWO_DAYS_AGO = date(year=2023, month=6, day=9)
 
 LONG_AGO = date.min
-DISTANT_FUTURE = date.max
 
-ONE_DAY = timedelta(days=1)
 SIX_DAYS = timedelta(days=6)
 ONE_WEEK = timedelta(weeks=1)
-TWO_WEEKS = timedelta(weeks=2)
 
 pytestmark = pytest.mark.freeze_time(TODAY)
 
 
-def test_makes_counts_for_mondays_between_repo_creation_and_now():
+def test_makes_counts_for_every_day_between_repo_creation_and_now():
     prs = {
-        repo("an-org", "a-repo", created_on=TODAY - TWO_WEEKS): [
+        repo("an-org", "a-repo", created_on=TWO_DAYS_AGO): [
             {"author": "an-author"},
         ]
     }
 
     counts = calculate_counts(prs, true)
     assert counts == {
-        ("an-org", "a-repo", "an-author", LAST_MONDAY - ONE_WEEK): 1,
-        ("an-org", "a-repo", "an-author", LAST_MONDAY): 1,
+        ("an-org", "a-repo", "an-author", TWO_DAYS_AGO): 1,
+        ("an-org", "a-repo", "an-author", YESTERDAY): 1,
+        ("an-org", "a-repo", "an-author", TODAY): 1,
     }
 
 
@@ -39,8 +38,8 @@ def test_drops_repos_after_archiving():
     repo_ = repo(
         "an-org",
         "a-repo",
-        created_on=TODAY - TWO_WEEKS,
-        archived_on=TODAY - ONE_WEEK,
+        created_on=TWO_DAYS_AGO,
+        archived_on=YESTERDAY,
     )
     prs = {
         repo_: [
@@ -50,20 +49,8 @@ def test_drops_repos_after_archiving():
 
     counts = calculate_counts(prs, true)
     assert counts == {
-        ("an-org", "a-repo", "an-author", LAST_MONDAY - ONE_WEEK): 1,
-    }
-
-
-def test_includes_archive_date_itself():
-    prs = {
-        repo("an-org", "a-repo", archived_on=LAST_MONDAY): [
-            {"author": "an-author"},
-        ]
-    }
-
-    counts = calculate_counts(prs, true)
-    assert counts == {
-        ("an-org", "a-repo", "an-author", LAST_MONDAY): 1,
+        ("an-org", "a-repo", "an-author", TWO_DAYS_AGO): 1,
+        ("an-org", "a-repo", "an-author", YESTERDAY): 1,
     }
 
 
@@ -77,7 +64,7 @@ def test_counts_prs():
     }
 
     counts = calculate_counts(prs, true)
-    assert counts == {("an-org", "a-repo", "an-author", LAST_MONDAY): 3}
+    assert counts == {("an-org", "a-repo", "an-author", TODAY): 3}
 
 
 def test_counts_only_prs_matching_predicate():
@@ -88,8 +75,8 @@ def test_counts_only_prs_matching_predicate():
         ]
     }
 
-    counts = calculate_counts(prs, lambda pr, _date: pr["merged_on"] == TODAY)
-    assert counts == {("an-org", "a-repo", "an-author", LAST_MONDAY): 1}
+    counts = calculate_counts(prs, lambda pr, _date: pr["merged_on"])
+    assert counts == {("an-org", "a-repo", "an-author", TODAY): 1}
 
 
 def test_returns_counts_by_org():
@@ -100,8 +87,8 @@ def test_returns_counts_by_org():
 
     counts = calculate_counts(prs, true)
     assert counts == {
-        ("an-org", "a-repo", "an-author", LAST_MONDAY): 1,
-        ("another-org", "another-repo", "an-author", LAST_MONDAY): 1,
+        ("an-org", "a-repo", "an-author", TODAY): 1,
+        ("another-org", "another-repo", "an-author", TODAY): 1,
     }
 
 
@@ -113,8 +100,8 @@ def test_returns_counts_by_repo():
 
     counts = calculate_counts(prs, true)
     assert counts == {
-        ("an-org", "a-repo", "an-author", LAST_MONDAY): 1,
-        ("an-org", "another-repo", "an-author", LAST_MONDAY): 1,
+        ("an-org", "a-repo", "an-author", TODAY): 1,
+        ("an-org", "another-repo", "an-author", TODAY): 1,
     }
 
 
@@ -128,8 +115,8 @@ def test_returns_counts_by_author():
 
     counts = calculate_counts(prs, true)
     assert counts == {
-        ("an-org", "a-repo", "an-author", LAST_MONDAY): 1,
-        ("an-org", "a-repo", "another-author", LAST_MONDAY): 1,
+        ("an-org", "a-repo", "an-author", TODAY): 1,
+        ("an-org", "a-repo", "another-author", TODAY): 1,
     }
 
 
@@ -151,19 +138,14 @@ def test_is_old():
     assert is_old(pr(created_on=LONG_AGO, closed_on=TODAY + ONE_WEEK), TODAY)
 
 
-def test_was_merged_in_week_ending():
-    # Unmerged PR is unmerged
-    assert not was_merged_in_week_ending(pr(merged_on=None), TODAY)
-
-    assert not was_merged_in_week_ending(pr(merged_on=LONG_AGO), TODAY)
-    assert not was_merged_in_week_ending(pr(merged_on=TODAY - ONE_WEEK), TODAY)
-    assert was_merged_in_week_ending(pr(merged_on=TODAY - SIX_DAYS), TODAY)
-    assert was_merged_in_week_ending(pr(merged_on=TODAY), TODAY)
-    assert not was_merged_in_week_ending(pr(merged_on=TODAY + ONE_DAY), TODAY)
-    assert not was_merged_in_week_ending(pr(merged_on=DISTANT_FUTURE), TODAY)
+def test_was_merged_in_on():
+    assert not was_merged_on(pr(merged_on=None), TODAY)
+    assert not was_merged_on(pr(merged_on=YESTERDAY), TODAY)
+    assert was_merged_on(pr(merged_on=TODAY), TODAY)
+    assert not was_merged_on(pr(merged_on=TOMORROW), TODAY)
 
 
-def repo(org, name, created_on=RECENTLY, archived_on=None):
+def repo(org, name, created_on=TODAY, archived_on=None):
     return FrozenDict(
         {
             "org": org,
